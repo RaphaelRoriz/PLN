46fcbf7b4c631c431d6e1d416c097f2ece066609
raphael f r roriz <raphael.roriz@estudante.ufla.br>
2019-10-30 22:43:25 -0300

    NluServices atualizado criando arquivo json

--- nlu/NluServices.py
+++ nlu/NluServices.py
@@ -1,11 +1,6 @@
-import requests 
-from bs4 import BeautifulSoup
-import matplotlib.pyplot as plt
-import seaborn as sns
 import os
 import spacy
 import pandas as pd
-import numpy as np
 import nltk
 from nltk.tokenize.toktok import ToktokTokenizer
 import re
@@ -15,16 +10,17 @@ import json
 
 
 nlp = spacy.load('pt', parse=True, tag=True, entity=True)
-#nlp_vec = spacy.load('en_vecs', parse = True, tag=True, #entity=True)
 tokenizer = ToktokTokenizer()
 stopword_list = nltk.corpus.stopwords.words('english')
 stopword_list.remove('no')
 stopword_list.remove('not')
 
+#pre processing function : remove the accented chars of the text
 def remove_accented_chars(text):
     text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
     return text
 
+#pre processing function : expand the contractions of the text using a the file contractions
 def expand_contractions(text,contraction_mapping = CONTRACTION_MAP):
 
   contractions_pattern = re.compile('({})'.format('|'.join(contraction_mapping.keys())),flags=re.IGNORECASE|re.DOTALL)
@@ -42,21 +38,25 @@ def expand_contractions(text,contraction_mapping = CONTRACTION_MAP):
   expanded_text = re.sub("'","",expanded_text)
   return expanded_text
 
+#pre processing function : remove the accented chars of the text
 def simple_stemmer(text):
   ps = nltk.porter.PorterStemmer()
   text = ' '.join([ps.stem(word) for word in text.split()])
   return text
 
+#pre processing function : remove the special chars of the text
 def remove_special_characters(text,remove_digits=False):
   pattern = r'[^a-zA-z0-9\s]' if not remove_digits else r'[^a-zA-z\s]'
   text = re.sub(pattern,'',text)
   return text
 
+#pre processing function : lemmatizes the text
 def lemmatize_text(text):
   text = nlp(text)
   text = ' '.join([word.lemma_ if word.lemma_ != '-PRON-' else word.text for word in text])
   return text
 
+#pre processing function : remove the stopwords of the text
 def remove_stopwords(text, is_lower_case=False):
     tokens = tokenizer.tokenize(text)
     tokens = [token.strip() for token in tokens]
@@ -67,7 +67,7 @@ def remove_stopwords(text, is_lower_case=False):
     filtered_text = ' '.join(filtered_tokens)    
     return filtered_text
 
-
+##pre processing function : normalize the text with chosen arguments
 def normalize_corpus(corpus,contraction_expansion = True,accented_char_removal=True,text_lower_case = True,text_lemmatization=True,special_char_removal=True,stopwords_removal=True,remove_digits=True):
 
   normalized_corpus = corpus
@@ -110,33 +110,49 @@ def normalize_corpus(corpus,contraction_expansion = True,accented_char_removal=T
 
   return normalized_corpus
 
-
-
+#input
 text = "este Texto aqui é um exemplo para ser@#  utilizado, durante o projeto ! nao é suposto que ele faça algum sentido"
+
 # pre-process text and store the same
 corpus = normalize_corpus(text , text_lower_case=False, 
                           text_lemmatization=False, special_char_removal=True)
-#print("a")
-#print(corpus)
-#print("b")
 
+#spacy nlp processing
 sentence_nlp = nlp(corpus)
 
 # POS tagging with Spacy 
-spacy_pos_tagged = [(word, word.pos_) for word in sentence_nlp]
-spacyPosDF = pd.DataFrame(spacy_pos_tagged, columns=['Word','Tag type'])
+spacy_pos_tagged = [(word.pos_) for word in sentence_nlp]
+#create a dataset with needed
+#spacyPosDF = pd.DataFrame(spacy_pos_tagged, columns=['Tag type'])
 
 #synthatic parsinh with spacy
-spacy_synthatic_parsed = [(word,word.dep_) for word in sentence_nlp]
-spacySynthaticDF = pd.DataFrame(spacy_synthatic_parsed, columns=['Word','Tag'])
+spacy_synthatic_parsed = [(word.dep_) for word in sentence_nlp]
+#create a dataset with needed
+#spacySynthaticDF = pd.DataFrame(spacy_synthatic_parsed, columns=['Tag'])
+
+#create dict to be converted for json 
+saida = {}
+saida['Tokens'] = []
+saida['Tokens'].append(tokenizer.tokenize(corpus))
+saida['POS Tagging'] = []
+saida['Synthatic'] = []
+saida['POS Tagging'].append(spacy_pos_tagged)
+saida['Synthatic'].append(spacy_synthatic_parsed)
+
+
+#creating/writing json
+with open('nlu.json','w',encoding='utf-8') as outfile:
+  json.dump(saida , outfile,ensure_ascii=False,indent = 4)
 
 
 # POS tagging with nltk , by now im just using spacy 
 #nltk_pos_tagged = nltk.pos_tag(text.split())
 #nltkDF = pd.DataFrame(nltk_pos_tagged, columns=['Word', 'POS tag'])
-print(text)
-print('Spacy POS: ')
-print(spacyPosDF.to_string())
-print('Spacy synthatic: ')
-print(spacySynthaticDF.to_string())
+
+#printing the normalized text and the tags datasets
+#print(text)
+#print('Spacy POS: ')
+#print(spacyPosDF.to_string())
+#print('Spacy synthatic: ')
+#print(spacySynthaticDF.to_string())
 
